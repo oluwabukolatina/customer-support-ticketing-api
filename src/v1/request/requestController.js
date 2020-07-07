0777864ab4bd9bf6d0fa6d0c5a325bc5c5468498
Tina Oluwabukola <oluwabukolatina@gmail.com>
2020-07-07 23:59:05 +0100

    save customer id

--- src/v1/request/requestController.js
+++ src/v1/request/requestController.js
@@ -3,14 +3,17 @@ const Request = require('../../../models/Request');
 module.exports = {
   async createRequest(req, res) {
     const { name } = req.body;
+    const { user } = req;
+    const { id } = user;
     if (!name) return res.status(400).json({ status: false, message: 'All fields required' });
 
     try {
       const newRequest = {
         name,
+        creator: id,
       };
       return Request.create(newRequest, (err, response) => {
-        if (err) return res.status(400).json({ message: 'Unable to create request'});
+        if (err) return res.status(400).json({ message: 'Unable to create request' });
         return res.status(200).json({ message: 'Request created!', request: response });
       });
     } catch (e) {
