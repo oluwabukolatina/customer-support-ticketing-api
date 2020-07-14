893cdffc436136b8a68abcbe427a81cdd8203f29
Tina Oluwabukola <oluwabukolatina@gmail.com>
2020-07-14 19:26:09 +0100

    refactor

--- src/v1/controllers/auth/auth.controller.ts
+++ src/v1/controllers/auth/auth.controller.ts
@@ -3,9 +3,11 @@ import { Response, Request } from 'express';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import { UserInterface } from '../../interface/user/user.interface';
+
 import UserService from '../../services/user/user.service';
 import config from '../../../config/config';
 import User from '../../models/User';
+import AuthService from '../../services/auth/auth.service';
 
 const saltRounds = 10;
 
@@ -28,51 +30,52 @@ class AuthController {
     } = req.body;
 
     if (!email) return res.status(400).send({ message: 'please enter your email', status: 'failed' });
-
     if (!password) return res.status(400).send({ message: 'please enter your password', status: 'failed' });
-
-    // check if user exists
-    const user: UserInterface = await UserService.getAUser(email);
-
-    if (user) return res.status(400).send({ message: 'user already exists', status: 'failed' });
-
-    const newUser = new User({
-      email,
-      password,
-      role,
-    });
-    return bcrypt.hash(password, saltRounds, (err, hash) => {
-      // Store hash in your DB.
-      if (err) throw err;
-
-      newUser.password = hash;
-      newUser.save()
-        .then((savedUser) => {
-          jwt.sign({
-            id: savedUser._id,
-            role: savedUser.role,
-            email: savedUser.email,
-          },
-          config.APP_JWT_SECRET,
-          { expiresIn: 3600000 },
-          (errToken, token) => {
-            if (errToken) throw errToken;
-            return res.status(201).json({
-              message: role ? 'admin created' : 'customer created',
-              user: {
-                id: savedUser._id,
-                email: savedUser.email,
-              },
-              token,
+    try {
+      const findUser = await UserService.getAUser(email);
+      if (findUser) return res.status(400).json({ message: 'User already exists', status: false });
+      const newUser = new User({
+        email,
+        password,
+        role,
+      });
+      return bcrypt.hash(password, saltRounds, (err, hash) => {
+        if (err) throw err;
+        newUser.password = hash;
+        newUser.save()
+          .then((savedUser) => {
+            jwt.sign({
+              id: savedUser._id,
+              role: savedUser.role,
+              email: savedUser.email,
+            },
+            config.APP_JWT_SECRET,
+            { expiresIn: 3600000 },
+            (errToken, token) => {
+              if (errToken) throw errToken;
+              return res.status(201).json({
+                message: role ? 'admin created' : 'customer created',
+                user: {
+                  id: savedUser._id,
+                  email: savedUser.email,
+                },
+                token,
+              });
             });
-          });
-        })
-        .catch(() => {
-          res.status(400).json({ message: 'Something went wrong!' });
-        });
-    });
+          })
+          .catch((e) => res.status(400).json({ message: 'Unable to create user', status: false }));
+      });
+    } catch (e) {
+      return res.status(400).json({ message: 'Something went wrong', status: false });
+    }
   }
 
+  /**
+   * @description login user
+   * @param {object} req
+   * @param {object} res
+   * @returns {object}
+   */
   static async loginUser(req: Request, res: Response) {
     const { body } = req;
     const {
